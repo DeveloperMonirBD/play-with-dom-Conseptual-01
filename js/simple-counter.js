e723a30de37a4a8ebb41cd961d41784d286beceb
Monir <mrmonir0558@gmail.com>
2024-09-18 18:24:59 +0600

    first commit

---
+++ js/simple-counter.js
@@ -0,0 +1,54 @@
+// // Read Operations: Option: 1
+// const count = document.getElementById('count');
+// // console.log(count.innerText);
+
+// function increaseCount() {
+//     let currentCount = Number(count.innerText);
+//     currentCount++;
+//     count.innerText = currentCount;
+// }
+// function decreaseCount() {
+//     let currentCount = Number(count.innerText);
+//     currentCount--;
+//     count.innerText = currentCount;
+// }
+
+
+// Read Operations: Option: 2
+const countSpan = document.getElementById("count")
+const counter = document.getElementById("counter")
+
+const increaseBtn = document.getElementsByClassName("increase-button")[0]
+const decreaseBtn = document.getElementsByClassName("decrease-button")[0]
+
+
+counter.addEventListener("click", function () {
+
+    if (increaseBtn.getAttribute("disabled") == null) {
+        console.log("Counter")
+        increaseBtn.setAttribute("disabled", true)
+        decreaseBtn.setAttribute("disabled", true)
+        counter.classList.add("bg-red-500")
+
+    } else {
+        increaseBtn.removeAttribute("disabled")
+        decreaseBtn.removeAttribute("disabled")
+        counter.classList.remove("bg-red-500")
+
+    }
+})
+
+increaseBtn.addEventListener("click", function (event) {
+    let currentCount = Number(countSpan.innerText)
+    currentCount++
+    countSpan.innerText = currentCount
+    event.stopPropagation()
+
+})
+decreaseBtn.addEventListener("click", function (event) {
+    let currentCount = Number(count.innerText)
+    currentCount--
+    countSpan.innerText = currentCount
+    event.stopPropagation()
+
+})
